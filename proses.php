64d23a0a44e5faf63ceb22705f844f833298429b
Diva Rizki M <71258418+DivaRizkiMegananda@users.noreply.github.com>
2020-09-28 23:05:23 +0700

    Kumpul tugas GET Diva Rizki M 6701190027

--- proses.php
+++ proses.php
@@ -1,1 +1,27 @@
 
+<?php
+if ($_GET) :
+    $bil1 = $_GET['bil1'];
+    $bil2 = $_GET['bil2'];
+    $operasi = $_GET['operasi'];
+
+    switch ($operasi) {
+        case 'tambah':
+            $hasil = $bil1 + $bil2;
+            break;
+        case 'kurang':
+            $hasil = $bil1 - $bil2;
+            break;
+        case 'kali':
+            $hasil = $bil1 * $bil2;
+            break;
+        case 'bagi':
+            $hasil = $bil1 / $bil2;
+            break;
+    }
+?>
+    <br>
+    Hasil: <strong><?php echo $hasil ?></strong>
+
+<?php
+endif; ?>
